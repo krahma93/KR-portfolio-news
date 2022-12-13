d3d96ea4e697875813d276a830f1c418727d2e35
krahma <khaled.rahma@kpmg.co.uk>
2022-12-13 12:09:34 +0000

    commit-madded select statement to only select items i want to return

--- models/articlesmodels.js
+++ models/articlesmodels.js
@@ -4,7 +4,7 @@ const db = require('../db/connection');
 
 exports.fetchArticles = () => {
    
-    return db.query(`SELECT articles.*,
+    return db.query(`SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes,
     (COUNT(comments.comment_id) ::INT) AS comment_count
     FROM articles
     LEFT JOIN comments ON comments.article_id = articles.article_id
